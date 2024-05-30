7fb08f131d3f8df761892b6a832024e3ca840cf0
zjason25 <90489418+zjason25@users.noreply.github.com>
2024-05-30 15:41:19 -0700

    Add function skeletons

---
+++ functions.js
@@ -0,0 +1,19 @@
+function getComputerChoice() {
+}
+
+function getHumanChoice() {
+
+}
+
+function playRound() {
+
+}
+
+function playGame() {
+
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+playGame(humanScore, computerScore);
